efef71bc768d33f6ad7c4034c21e61a65fe742ac
Vaclav Hummel <vaclav.hummel@student.tugraz.at>
2016-05-11 13:07:51 +0200

    Top milion webpages

--- src/modules/certcheck.py
+++ src/modules/certcheck.py
@@ -5,29 +5,25 @@ Check certificate.
 """
 
 import sys
+import os
 import StringIO
 import httplib
 import collections
 import logging
 import util
-import OpenSSL
+import ssl
+import csv
 
 log = logging.getLogger(__name__)
 
-destinations = [("www.google.com", 443)]
-DOMAIN, PORT = destinations[0]
+#destinations = [("www.google.com", 443)]
+#DOMAIN, PORT = destinations[0]
+
+PORT = 443
 
 
 # Mimic Tor Browser's request headers
 
-HTTP_HEADERS = [("Host", DOMAIN),
-                ("User-Agent", "Mozilla/5.0 (Windows NT 6.1; rv:38.0) "
-                               "Gecko/20100101 Firefox/38.0"),
-                ("Accept", "text/html,application/xhtml+xml,"
-                           "application/xml;q=0.9,*/*;q=0.8"),
-                ("Accept-Language", "en-US,en;q=0.5"),
-                ("Accept-Encoding", "gzip, deflate"),
-                ("Content-Length", "0")]
 
 
 
@@ -38,18 +34,27 @@ def readcert(exit_fpr):
 
     exit_url = util.exiturl(exit_fpr)
     log.debug("Probing exit relay \"%s\"." % exit_url)
+    print(exit_url)
 
-    conn = httplib.HTTPSConnection(DOMAIN, PORT, strict=False)
-    conn.request("GET", "/", headers=collections.OrderedDict(HTTP_HEADERS))
-    try:
-        response = conn.getresponse()
-    except Exception as err:
-        log.warning("urlopen() over %s says: %s" % (exit_url, err))
-        return
-    asn1cert = conn.sock.getpeercert(True)
-    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, asn1cert)
-    print(x509.get_subject().get_components())
+    with open('/home/xhumme00/Documents/TOR/exitmap/src/modules/top-1m.csv') as csvfile:
+        pagereader = csv.DictReader(csvfile, delimiter=',')
+        for page in pagereader:
+           DOMAIN=page['webpage']
+           print(DOMAIN)
+           HTTP_HEADERS = [("Host", DOMAIN), ("User-Agent", "Mozilla/5.0 (Windows NT 6.1; rv:38.0) " "Gecko/20100101 Firefox/38.0"), ("Accept", "text/html,application/xhtml+xml," "application/xml;q=0.9,*/*;q=0.8"), ("Accept-Language", "en-US,en;q=0.5"), ("Accept-Encoding", "gzip, deflate"), ("Content-Length", "0")]
+           try:
+               conn = httplib.HTTPSConnection(DOMAIN, PORT)
+               conn.request("GET", "/", headers=collections.OrderedDict(HTTP_HEADERS))
+               response = conn.getresponse()
+           except Exception as err:
+               print("\n")
+               #print(exit_url)
+               print(err)
+               print("\n")
+           print("OK")
 
+    print("Exit")
+   
 
 def probe(exit_desc, run_python_over_tor, run_cmd_over_tor, **kwargs):
     """
