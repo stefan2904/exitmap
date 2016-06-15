4091203732f53a518b83beb70811ca60c2e49eff
stefan <dev@2904.cc>
2016-06-15 15:10:58 +0200

    Cleaned up code

--- src/certutil.py
+++ src/certutil.py
@@ -7,7 +7,6 @@ by breaking the TLS connecting (SSLstrip) by using their own certificate.
 """
 
 import httplib
-import collections
 import logging
 import util
 import ssl
@@ -15,14 +14,19 @@ import csv
 import OpenSSL
 import requests
 
+# Set level of HTTP related loggers:
+logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(
+    level=logging.WARNING)
+
+logging.getLogger('requests.packages.urllib3.connection').setLevel(
+    level=logging.CRITICAL)
+
 log = logging.getLogger(__name__)
 
-# destinations = [("www.google.com", 443)]
-# DOMAIN, PORT = destinations[0]
 
 PORT = httplib.HTTPS_PORT  # 443
-
-LOGSSLERRORS = True
+TRUSTSTORE = 'mozillacerts.pem'
+LOGSSLERRORS = False
 
 
 def logCertError(domain, exitnode, error, fingerprint, exception, probeid):
@@ -30,8 +34,6 @@ def logCertError(domain, exitnode, error, fingerprint, exception, probeid):
         status = 'Fingerprint: %s' % fingerprint
     else:
         status = 'Exception:   %s' % exception
-
-    # TODO: log domain / exitnode / error / fingerprint to DB ?
 
     print('''
         Domain:      %s
@@ -42,7 +44,13 @@ def logCertError(domain, exitnode, error, fingerprint, exception, probeid):
           % (domain, exitnode, error, status))
 
 
-def handleCertError(err, page, probeid, exitnode, certErrorLogger):
+def handleCertError(
+        err,
+        page,
+        probeid,
+        exitnode,
+        certErrorLogger,
+        logSSLerrors):
     domain = str(page['webpage'])
     exitnode = str(exitnode)
     error = str(err)
@@ -56,12 +64,15 @@ def handleCertError(err, page, probeid, exitnode, certErrorLogger):
 
         fingerprint = str(x509Cert.digest('sha256'))
         if (fingerprint != page['fingerprint']):
+            print('%s: fingerprint mismatch found' % (domain))
             foundLogger(page, exitnode, fingerprint)
+        else:
+            print('%s: fingerprint OK' % (domain))
 
     except Exception as err:
         exception = str(err)
 
-    if LOGSSLERRORS:
+    if logSSLerrors:
         certErrorLogger(
             domain,
             exitnode,
@@ -72,10 +83,11 @@ def handleCertError(err, page, probeid, exitnode, certErrorLogger):
 
 
 def foundLogger(page, exitnode, fingerprint):
-
     fd = open('found.csv', 'a')
-    message = page['id'] + ',' + page['webpage'] + ',' + \
-        page['fingerprint'] + ',' + exitnode + ',' + fingerprint + '\n'
+    message = '%s,%s,%s,%s,%s\n' % (page['id'],
+                                    page['webpage'],
+                                    exitnode,
+                                    page['fingerprint'], fingerprint)
     fd.write(message)
     fd.close()
 
@@ -88,7 +100,7 @@ def foundLogger(page, exitnode, fingerprint):
           % (page['webpage'], exitnode, fingerprint, page['fingerprint']))
 
 
-def readCertOfPage(page, exitnode, certErrorLogger):
+def readCertOfPage(page, exitnode, certErrorLogger, logSSLerrors):
     """
     Read TLS certificate for the given page.
     Handle certificate errors accordingly.
@@ -96,50 +108,36 @@ def readCertOfPage(page, exitnode, certErrorLogger):
 
     domain = page['webpage']
     probeid = page['id']
-    HTTP_HEADERS = [
-        ('Host',
-         domain),
-        ('User-Agent',
-         'Mozilla/5.0 (Windows NT 6.1; rv:38.0) '
-         'Gecko/20100101 Firefox/38.0'),
-        ('Accept',
-         'text/html,application/xhtml+xml,'
-         'application/xml;q=0.9,*/*;q=0.8'),
-        ('Accept-Language',
-         'en-US,en;q=0.5'),
-        ('Accept-Encoding',
-         'gzip, deflate'),
-        ('Content-Length',
-         '0')]
+    http_headers = {
+        'Host':
+        domain,
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0',
+        'Accept':
+        'text/html,application/xhtml+xml,'
+        'application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language':
+        'en-US,en;q=0.5',
+        'Accept-Encoding':
+        'gzip, deflate',
+        'Content-Length':
+        '0'}
 
     try:
-        # c = ssl.create_default_context()
-        #c = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-        #c.verify_mode = ssl.CERT_REQUIRED
-        #c.check_hostname = True
-        #c.load_verify_locations(cafile='mozillacerts.pem', capath=None)
-
-        # TODO: Make sure only TorBrowser Certs are loaded (how?)
-
-        # print('Number loaded CA certs: %d' % len(c.get_ca_certs()))
-        # print(ssl.get_default_verify_paths())
-
-        # conn = httplib.HTTPSConnection(
-        #    domain, PORT, context=c)
-        # conn.request(
-        #    'GET', '/', headers=collections.OrderedDict(HTTP_HEADERS))
-        # response = conn.getresponse()
-
-        TRUSTSTORE = 'mozillacerts.pem'
-        #TRUSTSTORE = 'letsencrypt/'
-
-        r = requests.get(
+        requests.get(
             'https://' + domain,
             timeout=2,
+            headers=http_headers,
             verify=TRUSTSTORE).text
 
     except requests.exceptions.SSLError as err:
-        handleCertError(err, page, probeid, exitnode, certErrorLogger)
+        handleCertError(
+            err,
+            page,
+            probeid,
+            exitnode,
+            certErrorLogger,
+            logSSLerrors)
     except requests.exceptions.Timeout as err:
         print('%s: timeout' % (domain))
     except Exception as err:
@@ -149,15 +147,11 @@ def readCertOfPage(page, exitnode, certErrorLogger):
 def readCert(
         exit_fpr=None,
         certErrorLogger=logCertError,
-        sitelist='filtered.csv'):
+        sitelist='filtered.csv',
+        logSSLerrors=LOGSSLERRORS):
     """
     Read TLS certificates for all domains in sitelist.
     """
-
-    # TODO: only iterate over 'interesting' sites (filtered.csv)
-
-    # sitelist = 'top-1m.csv'
-    # sitelist = 'special.csv'
 
     exit_url = util.exiturl(exit_fpr) if exit_fpr is not None else '<noTOR>'
     log.debug('Probing exit relay \"%s\".' % exit_url)
@@ -166,4 +160,4 @@ def readCert(
         for page in csv.DictReader(csvfile, delimiter=','):
             if exit_fpr is None:
                 print('Scanning %s ...' % page['webpage'])
-            readCertOfPage(page, exit_fpr, certErrorLogger)
+            readCertOfPage(page, exit_fpr, certErrorLogger, logSSLerrors)
